24b86bde14a5a8b6d04216d8fa06d10c5826a27b
Tris Forster <tris@tfconsulting.com.au>
2014-04-15 09:36:55 +1000

    Fixed repo regexp

--- pycart/app.py
+++ pycart/app.py
@@ -5,10 +5,12 @@ Created on 25/11/2013
 '''
 import web
 
+repo = r'/repo/([\w\/\-]+)'
+
 urls = (
-    '/repo/([\w\/]+)/tree/([^/]+)/(.*)', 'git_repo.TreeView',
-    '/repo/([\w\/]+)/history/([^/]+)/(.*)', 'git_repo.HistoryView',
-    '/repo/([\w\/]+)/commit/(\w+)', 'git_repo.CommitView',
+    repo + '/tree/([^/]+)/(.*)', 'git_repo.TreeView',
+    repo + '/history/([^/]+)/(.*)', 'git_repo.HistoryView',
+    repo + '/commit/(\w+)', 'git_repo.CommitView',
     '/', 'git_repo.ListView',
 )
 app = web.application(urls, globals())
